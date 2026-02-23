71001998215038d6fde15745353c97d1bda85740
Noppakalo Wuttipaisankun <oatznpk@MacBook-Pro-khxng-Noppakalo.local>
2026-02-23 08:41:34 +0700

    add:deploy.yml in project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,53 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
 
-    modules: [
-        '@nuxt/eslint',
-        '@nuxt/hints',
-        '@nuxt/image',
-        '@nuxt/ui',
-        '@nuxt/scripts',
-        '@nuxtjs/google-fonts',
-    ],
-    css: ['~/assets/css/main.css'],
-    ui: {
-        colorMode: false,
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@nuxt/scripts",
+    "@nuxtjs/google-fonts",
+  ],
+  css: ["~/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
+  devtools: {
+    enabled: false,
+    timeline: {
+      enabled: false,
     },
-    devtools: {
-        enabled: false,
-        timeline: {
-            enabled: false,
-        },
+  },
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
     },
-    components: [
-        {
-            path: '~/components',
-            pathPrefix: false,
-        },
-    ],
-    googleFonts: {
-        families: {
-            'Plus+Jakarta+Sans': [400, 500, 700, 800],
-        },
+  ],
+  googleFonts: {
+    families: {
+      "Plus+Jakarta+Sans": [400, 500, 700, 800],
     },
-    router: {
-        options: {
-            scrollBehaviorType: 'smooth', // ทำให้เลื่อนแบบนุ่มนวลทั้งโปรเจกต์
-        },
+  },
+  router: {
+    options: {
+      scrollBehaviorType: "smooth",
     },
-    app: {
-        baseURL: '/portfolio/',
-        buildAssetsDir: 'assets', // ป้องกันปัญหาโฟลเดอร์ _nuxt ที่ GitHub Pages อาจจะบล็อก
-    },
-
-    ssr: false, // แนะนำให้ปิด SSR เป็น Single Page App (SPA) สำหรับ GitHub Pages
-
-    nitro: {
-        preset: 'github-pages',
-        prerender: {
-            crawlLinks: false,
-            routes: ['/404.html', '/200.html'],
-        },
-    },
+  },
 });
